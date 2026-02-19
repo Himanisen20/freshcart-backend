0d6fdf84f6b928c1b65f7f4cafed8b7d87c6ac9e
Himanisen20 <Himanisen20.himanisen69@gmail.com>
2026-02-19 13:25:14 +0530

    s

--- server.js
+++ server.js
@@ -21,8 +21,14 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//mongodb..... mongodb+srv://himanisen:21IuXeaeblT2T7Ns@cluster0.vz6mqyn.mongodb.net/ 
-mongoose.connect("mongodb://localhost:27017/freshcart").then(
+//mongodb..... 
+// mongoose.connect("mongodb://localhost:27017/freshcart").then(
+//     () => { console.log("mongoose connected") }
+// ).catch((error) => {
+//     console.log(error)
+// })
+
+mongoose.connect("mongodb+srv://himanisen:21IuXeaeblT2T7Ns@cluster0.vz6mqyn.mongodb.net/freshcart").then(
     () => { console.log("mongoose connected") }
 ).catch((error) => {
     console.log(error)
